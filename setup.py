99435368415d418aa9064e0cc0b6e1b196c3dc86
Michał Domański <michal.domanski@sensisoft.com>
2010-03-14 16:20:40 +0100

    simple moved all the files so one can use now setup script with insol :)

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='insol',
+      version='0.1.0',
+      description='insol: Solr search server API for python',
+      author='Michael Domanski',
+      author_email='mdomans@gmail.com',
+      packages=['insol'],
+     )
